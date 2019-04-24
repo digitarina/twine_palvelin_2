36cadb83a6e64a791cf6014bdc34d75d0ce031d5
root <root@vps.server.local>
2019-04-24 20:08:25 +0000

    controller.js update

--- controller.js
+++ controller.js
@@ -10,38 +10,41 @@
 
 	const models = require('./models');
 
-	exports.parseAndSave = (req, res)=>{
+	module.exports.parseAndSave = (req, res)=>{
 
 		// find tw-storydata html element
 		let $ = cheerio.load(req.text, { decodeEntities: false })
 		let storyElement = $.html('tw-storydata')
 
-                var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                let fileId = Array.apply(null, Array(8)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
-//		let fileId =  Math.random().toString(36).substring(10);
+		var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		let fileId = Array.apply(null, Array(8)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
+
 		let fileName = './story_files/' + fileId + '.html';
 
 		let script =`<script>
-						localStorage.setItem('new-story','` + fileId + `')
-						window.location.replace('http://195.110.58.110')
+						localStorage.setItem('new-story','` + fileId + `');
+						window.location.replace('http://localhost');
 					</script>`;
 					
 		let htmlString = storyElement.concat(script);
-
 		//save as html file and send link in response
+//		fs.writeFile(fileName, htmlString, (err) => {
 		fs.writeFile(fileName, req.text, (err) => {
 				if (err) throw err;
 				console.log('The file has been saved!');
-				res.send('http://31.220.111.30/story/' + fileId);
+				res.send('http://localhost/story/' + fileId);
 		});
 
 	}
 
-	exports.sendStory = (req, res)=>{
+	module.exports.sendStory = (req, res)=>{
+		console.log('Get story!');
+		var path = require('path');
 		if(!req.params.storyId){
 			return res.sendStatus(400);
 		}
-		res.sendFile(req.params.storyId + '.html', {root: __dirname + /story_files/});
+		res.sendFile(req.params.storyId + '.html', { root: path.join(__dirname, '/story_files') });
+//		res.sendFile(req.params.storyId + '.html', {root: __dirname + /story_files/});
 
 	}
 
